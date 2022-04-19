bdb7b6045c8d205f099efbd3e34b9918d99bd069
PappAdam <papp.adam@students.jedlik.eu>
2022-04-19 17:18:03 +0200

    XD (better colors, delay on scroll while zooming out, card design fixes) still hate the language btw

--- Scripts/transitions.js
+++ Scripts/transitions.js
@@ -1,6 +1,13 @@
 const layers = document.getElementsByClassName("parlayer");
 
 function fullScreen() {
+    if (history.scrollRestoration) {
+        history.scrollRestoration = 'manual';
+    } else {
+        window.onbeforeunload = function () {
+            window.scrollTo(0, 0);
+        }
+    }
     alert("NYOMD MEG AZt a kurVA F11-t\n koszi :)")
 }
 
@@ -12,6 +19,12 @@ function zoomOut() {
         layers[i].style.left = "0";
     }
     document.getElementById("shadow").style.opacity = "0";
-    document.body.style.overflowY = "auto";
-    document.body.style.overflowX = "hidden";
+    setTimeout(function () { 
+        document.body.style.overflowY = "auto";
+        document.body.style.overflowX = "hidden";
+    },
+    1000);
+}
+
+window.onbeforeunload = function () {
 }
